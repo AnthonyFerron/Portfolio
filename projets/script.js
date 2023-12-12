3d7e24edfe9484aa354bc2545a4ebae14eaf2bb0
AnthonyFerron <anthony.ferron74@gmail.com>
2023-12-12 15:48:11 +0100

    aa

--- projets/script.js
+++ projets/script.js
@@ -6,37 +6,12 @@ const btns=[
 },
 {
     id: 2,
-    name: 'Classique',
+    name: 'Clients',
     tag: 'true'
 },
 {
     id: 3,
-    name: 'Bd',
-    tag: 'true'
-},
-{
-    id: 4,
-    name: 'Manga',
-    tag: 'true'
-},
-{
-    id: 5,
-    name: 'Biographie',
-    tag: 'true'
-},
-{
-    id: 6,
-    name: 'Enfant',
-    tag: 'true'
-},
-{
-    id: 7,
-    name: 'Fantastique',
-    tag: 'true'
-},
-{
-    id: 8,
-    name: 'Polar',
+    name: 'Perso',
     tag: 'true'
 },
 ]
@@ -48,11 +23,11 @@ document.getElementById('btns').innerHTML=filters.map((btn)=>{
     var {name, id, tag} = btn;
     if(id=='1'){
         return(
-            "<button class='flex justify-center items-center bg-gradient-to-b from-amber-500 shadow backdrop-blur rounded-[30px] text-3xl w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='document.location.reload("+(tag)+`)'>${name}</button>`
+            "<button class='flex justify-center items-center bg-gradient-to-b from-blue-500 shadow backdrop-blur rounded-[30px] text-3xl  w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='document.location.reload("+(tag)+`)'>${name}</button>`
     )}
     else{
         return(
-            "<button class='flex justify-center items-center bg-gradient-to-b from-amber-500 shadow backdrop-blur rounded-[30px] text-3xl w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='filterItems("+(id)+`)'>${name}</button>`
+            "<button class='flex justify-center items-center bg-gradient-to-b from-blue-500 shadow backdrop-blur rounded-[30px] text-3xl md:mt-0 mt-3  w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='filterItems("+(id)+`)'>${name}</button>`
         )
     }
 }).join('');

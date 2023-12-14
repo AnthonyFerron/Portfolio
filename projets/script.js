e2fdafdda8713c860b6fc77e249b4f7d9c948c11
AnthonyFerron <anthony.ferron74@gmail.com>
2023-12-14 16:52:10 +0100

    aa

--- projets/script.js
+++ projets/script.js
@@ -38,7 +38,7 @@ const product = [
     image: '../icons/librairie.png',
     title: 'Librairie',
     link: 'https://ferron.labo-g4.fr/librairie/',
-    content: "Ce projet avait pour but de créer une identité visuelle complète pour une cliente. Cela allait du nom au site web finis, en passant par le logo et la maquette. Ce projet m’aura apporté des connaissances en PAO, ainsi qu’une expérience en tant que chef de projet, et une expérience de relation client. Ce site est donc un site vitrine pour une toute nouvelle librairie ayant ouvert le 8 décembre 2023. Notre but, par équipe de 3, était de promouvoir cette librairie a l’aide d’une vitrine commerciale forte. Nous avons également créé un stop-trottoir, un flyer, ainsi qu’un marque page aux couleurs de la librairie.",
+    content: "Ce projet avait pour but de créer une identité visuelle complète pour une cliente. Cela allait du nom au site web finis, en passant par le logo et la maquette. Ce projet m'aura apporté des connaissances en PAO, ainsi qu'une expérience en tant que chef de projet, et une expérience de relation client. Nous avons dû apprendre de nouveaux langages tel que tailwind, javascript et php, qui nous serviront pour sur dans les prochains projets. Ce site est donc un site vitrine pour une toute nouvelle librairie ayant ouvert le 8 décembre 2023. Notre but, par équipe de 3, était de promouvoir cette librairie a l'aide d'une vitrine commerciale forte. Nous avons également créé un stop-trottoir, un flyer, ainsi qu'un marque page aux couleurs de la librairie.",
     warning: 'Attention, site uniquement accessible sur PC'
 },
 {
@@ -46,16 +46,16 @@ const product = [
     image: '../icons/portfolio.png',
     title: 'Portfolio',
     link: 'https://ferron.labo-g4.fr/',
-    content: "",
+    content: "Le Portfolio sur lequel vous êtes actuellement, est un projet réalisé en partie en cours, mais surtout sur mon temps libre. Ce projet a été créé dans le but de me présenter auprès des recruteurs ou même des autres développeurs. Les principales fonctions sont donc la présentation de mes projets et les possibilités de contact. Ce site a été construit avec le framework HTML Tailwind Css. Il y a également du PHP et du Javascript pour la gestion du formulaire de contact ou du filtre dans la page projet.",
     warning: "Site accessible sur téléphone et PC"
 },
 {
     id: 2,
-    image: '../icons/bde.jpg',
+    image: '../icons/bde.png',
     title: 'BDE G4',
     link: 'https://ferron.labo-g4.fr/G4-BDE/',
     content : "Dans ce projet par équipes de 4, nous devions créer le site du BDE de notre école. Ce site avait pour but de créer un moyen de communication et d'annonce rapide et efficace pour leurs événements. Il nous fallait donc un design simple, présentant les membres du BDE ainsi que les événements à venir. Nous avons également pris la liberté de créer une page souvenir, nous rappelant à tous les meilleurs moments passés dans leurs événements.",
-    warning: ""
+    warning: "Site accessible sur téléphone et PC"
 },
 ];
 
@@ -79,10 +79,10 @@ const displayItem = (items) => {
     {
         var{image, title, link, content} = item;
         return(
-            `<div class="flex flex-col justify-center items-center bg-gradient-to-r from-blue-600 to-blue-500 md:w-[32%] h-full mt-4 ml-[1%] mr-[1%] md:mr-0 rounded-[50px] shadow-blue-700 shadow-xl">
+            `<div class="flex flex-col justify-center items-center bg-gradient-to-r from-blue-600 to-blue-500 md:w-[32%] h-auto mt-4 ml-[1%] mr-[1%] md:mr-0 rounded-[50px] shadow-blue-700 shadow-xl">
             <h1 class="text-2xl mt-2 mb-5">${title}</h1>
             <img class="rounded-[20px] p-2" src="${image}">
-            <p class="p-5 mt-[-5px]">${content}</p>
+            <p class="p-5 mt-[-5px] text-white text-justify">${content}</p>
             <a href="${link}" target="_blank"><button class="text-white">${title}</button></a>
             </div>
 
